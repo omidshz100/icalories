6868ed84cd7c17ea9d35d1060bdf4d4f66ae00e6
Omid Shojaeian Zanjani <46863450+omidshz100@users.noreply.github.com>
2023-05-06 17:09:20 +0200

    Initial Commit

---
+++ iCalories/iCaloriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCaloriesApp.swift
+//  iCalories
+//
+//  Created by Omid Shojaeian Zanjani on 06/05/23.
+//
+
+import SwiftUI
+
+@main
+struct iCaloriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
