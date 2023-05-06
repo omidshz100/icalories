386f6811122bdf9ad0ac5bcde82d83933c341b84
Omid Shojaeian Zanjani <46863450+omidshz100@users.noreply.github.com>
2023-05-06 18:39:44 +0200

    the function calculate the time 2 minutes, 2 hours or 2 days ago

--- iCalories/Helpers/TimeFormatting.swift
+++ iCalories/Helpers/TimeFormatting.swift
@@ -8,3 +8,19 @@
 import Foundation
 
 
+func calTimeSince(date:Date) -> String{
+    let min = Int(-date.timeIntervalSinceNow) / 6_0000
+    let hours = min / 60
+    let days = hours / 24
+    
+    if min < 120 {
+        return "\(min) minutes ago"
+    }else if min >= 120 && hours < 24 {
+        return "\(hours) hours ago"
+    }else{
+        return "\(days) days ago"
+    }
+}
+
+
+
